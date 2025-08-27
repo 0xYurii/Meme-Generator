5aef4fe5c0459ba4b6985ff11ed59700566a76ec
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-28 00:27:02 +0100

    feat: hands practis on useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
+import WindowTracker from "./WindowTracker"
 
 export default function App() {
+    /**
+     * Challenge:
+     * 1. Create state called `show`, default to `true`
+     * 2. When the button is clicked, toggle `show`
+     * 3. Only display `<WindowTracker>` if `show` is `true`
+     */
     
     return (
-        <>
-            <Header />
-            <Main />
-        </>
+        <main className="container">
+            <button>
+                Toggle WindowTracker
+            </button>
+            <WindowTracker />
+        </main>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,19 @@
-import { useState } from "react"
-
+import { useState,useEffect } from "react"
 export default function Main() {
     const [meme, setMeme] = useState({
         topText: "One does not simply",
         bottomText: "Walk into Mordor",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
+    const [allMemes, setAllMemes] = useState([])
     
-    /**
-     * Challenge:
-     * Get an array of memes from the imgflip API as soon as
-     * this component renders for the first time.
-     * Check the imgflip documentation for the correct URL.
-     * Save the array of memes (not the whole response
-     * data) to state. (For this app, we'll randomly choose
-     * one of the memes from this array when the user clicks
-     * the "Get a new meme image" button, but we'll do that in
-     * a separate challenge.)
-     * 
-     * Hint: for now, don't try to use an async/await function.
-     * Instead, use `.then()` to resolve the promises
-     * from using `fetch`. We'll learn why after this challenge.
-     */
-    
-    
+    useEffect(async()=>{
+        const res=await fetch("https://api.imgflip.com/get_memes")
+        const data=await res.json()
+        setAllMemes(data.data.memes)
+    },[])
+
+
     
     function handleChange(event) {
         const {value, name} = event.currentTarget
