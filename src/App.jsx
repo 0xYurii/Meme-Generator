a52449398de665a4c73915c501923e9c52fdb890
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-27 16:34:20 +0100

    feat: learn useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
+import React from "react"
 
 export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+    
+    console.log("Rendered!")
+    
+    React.useEffect(()=>{
+      console.log("Effect function ran")
+    },[count])
     
     return (
-        <>
-            <Header />
-            <Main />
-        </>
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
     )
 }
