a53c8d3490f0cc72aa12087d216a83a4d0340f53
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-28 09:36:50 +0100

    feat: add more functionality using states and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import WindowTracker from "./WindowTracker"
-
+import { useState } from "react"
 export default function App() {
-    /**
-     * Challenge:
-     * 1. Create state called `show`, default to `true`
-     * 2. When the button is clicked, toggle `show`
-     * 3. Only display `<WindowTracker>` if `show` is `true`
-     */
-    
+
+    const[show,setShow]=useState(true)
+    function toggle(){
+        setShow(!show)
+    }
     return (
         <main className="container">
-            <button>
+            <button onClick={toggle}>
                 Toggle WindowTracker
             </button>
-            <WindowTracker />
+            {show&&<WindowTracker />}
         </main>
     )
 }
