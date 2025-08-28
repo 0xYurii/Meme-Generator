c65f3d3b44dfff533f21ddb7b0c4546af6bc8aa4
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-28 09:48:30 +0100

    feat: cleaning up the code

--- src/WindowTracker.jsx
+++ src/WindowTracker.jsx
@@ -1,20 +1,17 @@
 import React from "react"
 
 export default function WindowTracker() {
-    /**
-     * Challenge:
-     * 1. Create state called `windowWidth`, default to 
-     *    `window.innerWidth`
-     * 2. When the window width changes, update the state
-     * 3. Display the window width in the h1 so it updates
-     *    every time it changes
-     */
     const[windowWidth,setWindowWidth]=React.useState(window.innerWidth)
 
+    
     React.useEffect(() => {
-        window.addEventListener("resize", function() {
-            setWindowWidth(prev=>window.innerWidth)
-        })
+        function watchWindowWidth(){
+            setWindowWidth(window.innerWidth)
+        }
+        window.addEventListener("resize", watchWindowWidth)
+        return function(){
+            window.removeEventListener("resize",watchWindowWidth)
+        }
     }, [])
     
     return (
